018caf7a2333136a6b7478fe66b5d3fbccc54614
Michael Bishop <miklb@miklb.com>
2011-09-16 12:18:24 -0400

    basic sidebar structure. No default content, just support for blocks

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,6 @@
+<?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
 	<aside>
-		
+		<?php Plugins::act( 'theme_sidebar_top' ); ?>
+		<?php $theme->area( 'sidebar' ); ?>
+		<?php Plugins::act( 'theme_sidebar_bottom' ); ?>
 	</aside>
