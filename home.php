3ae3c89f5544b2dabcfbff73e193e70f81e7db6e
Michael Bishop <miklb@miklb.com>
2011-09-16 11:37:01 -0400

    base home loop/structure

--- home.php
+++ home.php
@@ -1,7 +1,28 @@
 <?php $theme->display ( 'header' ); ?>
 
 		<div id="primary-content">
-			
+			<?php foreach ( $posts as $post ) { ?>
+				
+				<article id="post-<?php echo $post->id; ?>" class="<?php echo $post->statusname; ?>">
+					<header>								
+						<h1><a href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h1>
+						<time datetime="<?php echo $post->pubdate_datetime; ?>"><?php echo $post->pubdate_out; ?></time>
+					</header>
+
+						<?php echo $post->content_out; ?>
+
+					<footer>
+						<?php if ( count( $post->tags ) > 0 )  { ?>
+						<div class="tags"><?php _e('Tagged:'); ?> <?php echo $post->tags_out; ?></div>
+						<?php } ?>
+						<div class="commentCount"><?php $theme->comments_link($post,'%d Comments','%d Comment','%d Comments'); ?></div>
+						
+					</footer>
+				</article>
+			<?php }  ?>
+			<nav id="pagination">
+				<?php $theme->prev_page_link('&laquo; ' . _t('Previous Page')); ?> <?php $theme->page_selector( null, array( 'leftSide' => 2, 'rightSide' => 2 ) ); ?> <?php $theme->next_page_link('&raquo; ' . _t('Next Page')); ?>
+			</nav>
 		</div>
 		
 <?php $theme->display ( 'sidebar' ); ?>
