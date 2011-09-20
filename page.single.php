00f971de0f72eae45afc336e94393602110304b6
Michael Bishop <miklb@miklb.com>
2011-09-20 14:40:09 -0400

    page template

---
+++ page.single.php
@@ -0,0 +1,14 @@
+<?php $theme->display ( 'header' ); ?>
+
+			<section id="content">
+				<article id="post-<?php echo $post->id; ?>" class="<?php echo $post->statusname; ?>">
+					<header>								
+						<h1><a href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h1>
+					</header>
+
+						<?php echo $post->content_out; ?>
+						
+					
+				
+				</article>
+			</section>
