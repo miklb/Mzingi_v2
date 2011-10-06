cae2704636a5e1c5611d602bf4f4f6330e7c7c84
Michael Bishop <miklb@miklb.com>
2011-10-06 15:32:33 -0400

    adding an id to main footer so article footers don't get same style applied

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-	<footer>
+	<footer id="colophon">
 		<p>Powered by <a href="http://habariproject.org">Habari</a>. Built on the <a href="https://github.com/miklb/Mzingi_v2">Mzingi 2</a> framework.</p>
 		
 		<?php echo $theme->footer(); ?>
