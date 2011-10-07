2cd7d869143f34fea031344ddc7c076f22e81323
Michael Bishop <miklb@miklb.com>
2011-10-06 20:40:18 -0400

    correct section id for page templates

--- page.single.php
+++ page.single.php
@@ -1,6 +1,6 @@
 <?php $theme->display ( 'header' ); ?>
 
-			<section id="content">
+			<section id="primary-content">
 				<article id="post-<?php echo $post->id; ?>" class="<?php echo $post->statusname; ?>">
 					<header>								
 						<h1><a href="<?php echo $post->permalink; ?>" title="<?php echo $post->title; ?>"><?php echo $post->title_out; ?></a></h1>
