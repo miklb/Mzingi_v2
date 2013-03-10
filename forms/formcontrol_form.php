fe871b327f4f7a6b3e7fd8364111277aff02caa8
Michael Bishop <miklb@miklb.com>
2013-03-09 21:20:31 -0500

    update formcontrol_form to match core. Probably all of the form controls are outdated

--- forms/formcontrol_form.php
+++ forms/formcontrol_form.php
@@ -1,13 +1,24 @@
-<form
-	id="<?php echo $id; ?>"
-	method="post"
-	action="<?php echo $action; ?>"
-	class="<?php echo $class; ?>"
-	enctype="<?php echo $enctype; ?>"
-	<?php echo $onsubmit; ?>
->
-<?php if(isset($message) && $message != ''): ?>
-<p><?php echo $message; ?></p>
+<?php if ( !defined( 'HABARI_PATH' ) ) { die('No direct access'); } ?>
+<form <?php
+	$fixed = array(
+		'method' => 'POST',
+	);
+	if($form->disabled) {
+		$fixed['disabled'] = 'disabled';
+	}
+	echo $form->parameter_map(
+		array(
+			'class',
+			'id',
+			'action',
+			'enctype',
+			'accept-charset' => 'accept_charset',
+			'onsubmit',
+		),
+		$fixed
+	); ?>>
+<?php if (isset($message) && $message != ''): ?>
+<div class="form_message"><?php echo $message; ?></div>
 <?php endif; ?>
 <input type="hidden" name="FormUI" value="<?php echo $salted_name; ?>">
 <?php echo $pre_out; ?>
